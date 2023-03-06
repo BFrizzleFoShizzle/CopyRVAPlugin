c1f435fd02a6f387e1ef8ea02f4605aa4ba172da
Bryden Frizzell <bryden.frizzell@gmail.com>
2023-03-06 13:01:17 +1300

    Cleanup

--- CopyRVAPlugin.cpp
+++ CopyRVAPlugin.cpp
@@ -13,36 +13,7 @@ void CopyRVAPlugin::setupPlugin() {}
 static void CopyRVAToClipboard(uintptr_t address)
 {
     CutterCore* core = Core();
-    /*
-    // none of these seem to be populated
-    auto relocs = core->getAllRelocs();
-    for (auto reloc : relocs)
-        core->message("Reloc: " + reloc.name + " " + QString::number(reloc.paddr) + " " + QString::number(reloc.vaddr));
-    auto mapDescriptions = core->getMemoryMap();
-    for (auto mapDescription : mapDescriptions)
-        core->message("Map description: " + mapDescription.name + " "  + mapDescription.fileName  + " " + QString::number(mapDescription.addrStart, 16) + " " + QString::number(mapDescription.addrEnd, 16));
-    auto segments = core->getAllSegments();
-    for (auto segment : segments)
-        core->message("Segment: " + segment.name + " " + QString::number(segment.paddr, 16) + " " + QString::number(segment.vaddr, 16));
-    auto sections = core->getAllSections();
-    // this doesn't give you the base address...
-    for (auto section : sections)
-    {
-        core->message("Section: " + section.name + " " + QString::number(section.paddr, 16) + " " + QString::number(section.vaddr, 16));
-        if (section.vaddr <= address && address < section.vaddr + section.size)
-        {
-            QClipboard* clipboard = QApplication::clipboard();
-            size_t fileOffset = (address - section.vaddr) + section.paddr;
-            // leading '+' to make it obvious it's an offset
-            // TODO setting to remove '+'
-            clipboard->setText("+0x" + QString::number(fileOffset, 16));
-            return;
-        }
-    }
-    */
     
-    //core->message("Base: " + QString::number(rz_bin_get_baddr(core->core()->bin), 16));
-
     QClipboard* clipboard = QApplication::clipboard();
     size_t fileOffset = address - rz_bin_get_baddr(core->core()->bin);
     if (fileOffset > rz_bin_get_size(core->core()->bin))
@@ -92,7 +63,6 @@ void CopyRVAPlugin::setupInterface(MainWindow *main)
 
     if(!con1 || !con2 || !con3)
         Core()->message("CopyRVAPlugin: Connection failed");
-
 
     Core()->message("CopyRVAPlugin: loaded");
 }
